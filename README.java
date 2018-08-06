2fc21cf95057022249ef07860a75b283cac64fd7
naveennavi46 <42136125+naveennavi46@users.noreply.github.com>
2018-08-06 08:36:48 +0000

    Create README.java

---
+++ README.java
@@ -0,0 +1,15 @@
+# positive-or-not
+import java.utill.*;
+class Positive
+{
+public static void main(string args[])
+{
+int num=3;
+system.out.println("the integer is");
+if(num>0)
+system.out.println("the number is positive");
+else if(num<0)
+system.out.println("the number is negative");
+else
+system.out.println("the number is zero");
+}
